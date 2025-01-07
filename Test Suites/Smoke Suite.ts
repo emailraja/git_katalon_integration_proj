d50d0e6dc5cc5e0fb438f1964b555b005ae17598
emailraja <email.raja.10.4@gmail.com>
2025-01-07 00:54:04 -0500

    Commiting smoke test and suite

---
+++ Test Suites/Smoke Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ca001b6d-572a-4b34-a13e-928cf1123b79</testSuiteGuid>
+</TestSuiteEntity>
