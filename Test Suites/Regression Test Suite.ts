ed92c71cc6ff0003d045fee312d708b28fa40223
emailraja <email.raja.10.4@gmail.com>
2025-01-07 17:19:53 -0500

    Regression Testcase and suite commited

---
+++ Test Suites/Regression Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>553e5239-c163-4dfd-8db3-7854b88b75e2</testSuiteGuid>
+</TestSuiteEntity>
